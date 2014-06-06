8705b8d27f3c65b41cedac03ebb06bca6dfa7dd6
Teodor Tunev <teodortunev@gmail.com>
2014-06-06 23:39:17 +0300

    Make Smart Generator :)

--- QuestionGenerator.js
+++ QuestionGenerator.js
@@ -2,69 +2,31 @@
     //In production with Start menu its good to be Async...
     jQuery.ajaxSetup({ async: false });
     var arrayWithQuestion = [];
-    var numberOfNeededQuestionForLevel = 5;
+
     var getRandomIndexFromArray = function (arrayLength) {
         var index = (Math.random() * arrayLength) | 0;
         return index;
     }
-    var getEasyQuestion = function () {
-        var arrayWithEasyQuestion = [];
-        $.getJSON("Questions/Level1.html", function (data) {
+
+    var getQuestionsFromFile = function (filePath , countOfNeededQuestions) {
+        var arrayWithChoosenQuestions = [];
+        $.getJSON(filePath, function (data) {
             var arrayWithQuestion = data;
-            for (var i = 0; i < numberOfNeededQuestionForLevel; i++) {
+            for (var i = 0; i < countOfNeededQuestions; i++) {
                 var dataLength = arrayWithQuestion.length;
                 var indexFromArray = getRandomIndexFromArray(dataLength);
-                arrayWithEasyQuestion.push(arrayWithQuestion[indexFromArray]);
+                arrayWithChoosenQuestions.push(arrayWithQuestion[indexFromArray]);
                 arrayWithQuestion.splice(indexFromArray, 1);
             }
         })
-        return arrayWithEasyQuestion;
-    }
-
-    var getMediumQuestion = function () {
-        var arrayWithMediumQuestion = [];
-        $.getJSON("Questions/Level2.html", function (data) {
-            var arrayWithQuestion = data;
-            for (var i = 0; i < numberOfNeededQuestionForLevel; i++) {
-                var dataLength = arrayWithQuestion.length;
-                var indexFromArray = getRandomIndexFromArray(dataLength);
-                arrayWithMediumQuestion.push(arrayWithQuestion[indexFromArray]);
-                arrayWithQuestion.splice(indexFromArray, 1);
-            }
-        })
-        return arrayWithMediumQuestion;
-    }
-
-    var getHardQuestion = function () {
-        var arrayWithHardQuestion = [];
-        $.getJSON("Questions/Level3.html", function (data) {
-            var arrayWithQuestion = data;
-            for (var i = 0; i < numberOfNeededQuestionForLevel; i++) {
-                var dataLength = arrayWithQuestion.length;
-                var indexFromArray = getRandomIndexFromArray(dataLength);
-                arrayWithHardQuestion.push(arrayWithQuestion[indexFromArray]);
-                arrayWithQuestion.splice(indexFromArray, 1);
-            }
-        })
-        return arrayWithHardQuestion;
-    }
-
-    var getBonusQuestion = function () {
-        var arrayWithBonusQuestion = [];
-        $.getJSON("Questions/Level4.html", function (data) {
-            var arrayWithQuestion = data;
-                var dataLength = arrayWithQuestion.length;
-                var indexFromArray = getRandomIndexFromArray(dataLength);
-                arrayWithBonusQuestion.push(arrayWithQuestion[indexFromArray]);
-        })
-        return arrayWithBonusQuestion;
+        return arrayWithChoosenQuestions;
     }
 
     var getQuestions = function () {
-        var arrWithEasy = getEasyQuestion();
-        var arrWithMedium = getMediumQuestion();
-        var arrWithHard = getHardQuestion();
-        var bonusQuestion = getBonusQuestion();
+        var arrWithEasy = getQuestionsFromFile("Questions/Level1.html", 5);
+        var arrWithMedium = getQuestionsFromFile("Questions/Level2.html", 5);
+        var arrWithHard = getQuestionsFromFile("Questions/Level3.html", 5);
+        var bonusQuestion = getQuestionsFromFile("Questions/Level4.html", 1);
         arrayWithQuestion = arrayWithQuestion.concat(arrWithEasy, arrWithMedium, arrWithHard , bonusQuestion);
         return arrayWithQuestion;
     };
