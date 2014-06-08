f83eb98ad514ee956758d1b2aa6afcd64182f9ab
kris4o1993 <kris4o1993@gmail.com>
2014-06-08 21:41:16 +0300

    2 minor fixes
    - fixed one of the questions;
    - numbers above timer set to 30-20-10-0 (instead of 0-10-20-30)

--- SVGRender.js
+++ SVGRender.js
@@ -4,10 +4,10 @@
         paper = Raphael(80, 370, 500, 100);
         progressBar = paper.rect(30, 50, 399, 50, 5);
         paper.setStart();
-        var zero = paper.text(30, 40, '0');
-        var ten = paper.text(169, 40, '10');
-        var twenty = paper.text(302, 40, '20');
-        var thirty = paper.text(430, 40, '30');
+        var zero = paper.text(30, 40, '30');
+        var ten = paper.text(169, 40, '20');
+        var twenty = paper.text(302, 40, '10');
+        var thirty = paper.text(430, 40, '0');
         var set = paper.setFinish();
         set.attr({
             'font-size': '20px',
