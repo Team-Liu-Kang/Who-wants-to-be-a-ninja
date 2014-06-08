f6065624ac0acbc55f08e93f2fa8b85c95621f61
Martin <sirjordan1988@gmail.com>
2014-06-08 19:47:03 +0300

    Answers correct/incorrect boxes
    Answers correct/incorrect boxes, and description

--- KineticRenderForCorrectAnswer.js
+++ KineticRenderForCorrectAnswer.js
@@ -1,14 +1,52 @@
 ﻿var KineticRenderForCorrectAnswer = function (stage) {
     var stage = stage;
     var layer = new Kinetic.Layer();
-    var correctAnswer = function () {
+    var correctAnswer = function (strCorrectAnswerDescription) {
         $('#container').fadeOut(1500);
+
+        if (strCorrectAnswerDescription === undefined) {
+            strCorrectAnswerDescription = 'Congratulations, you answer correctly.';
+        }
+
+        var corectAnswerBox = new Kinetic.Rect({
+            x: 150,
+            y: 150,
+            width: 500,
+            height: 300,
+            fill: '#00FF33',
+            stroke: 'darkgreen',
+            strokeWidth: 2,
+            opacity: 0.8
+        });
+
+        var corectAnswerText = new Kinetic.Text({
+            x: 150,
+            y: 200,
+            text: 'Answer Correct !',
+            fontSize: 35,
+            fontFamily: 'Calibri',
+            width: 500,
+            align: 'center',
+            fill: 'red',
+        });
+
+        var corectAnswerDescriptionText = new Kinetic.Text({
+            x: 150,
+            y: 240,
+            text: strCorrectAnswerDescription,
+            fontSize: 20,
+            fontFamily: 'Calibri',
+            width: 500,
+            align: 'center',
+            fill: '#1D7074',
+        });
+
         var nextQuestion = new Kinetic.Rect({
             x: 250,
             y: 500,
             width: 300,
             height: 70,
-            fill: 'red',
+            fill: 'green',
             stroke: 'black',
             strokeWidth: 5,
         });
@@ -45,13 +83,52 @@
             nextQuestion.fill('red');
             layer.draw();
         })
-        layer.add(nextQuestion, nextQuestionText, nextQuestionInvisible);
+        layer.add(nextQuestion, nextQuestionText, nextQuestionInvisible, corectAnswerBox, corectAnswerText, corectAnswerDescriptionText);
         stage.add(layer);
         
     };
 
-    var incorrectAnswer = function () {
+    var incorrectAnswer = function (strIncorrectAnswerDescription) {
+        if (strIncorrectAnswerDescription === undefined) {
+            strIncorrectAnswerDescription = 'You may have more luck next time.';
+        }
+
         $('#container').fadeOut(1500);
+
+        var incorectedAnswerBox = new Kinetic.Rect({
+            x: 150,
+            y: 150,
+            width: 500,
+            height: 300,
+            fill: 'orange',
+            stroke: 'red',
+            strokeWidth: 2,
+            align: 'center',
+            opacity: 0.8
+        });
+
+        var incorectAnswerText = new Kinetic.Text({
+            x: 150,
+            y: 200,
+            text: 'Incorrect Answer !',
+            fontSize: 35,
+            fontFamily: 'Calibri',
+            width: 500,
+            align: 'center',
+            fill: 'red',
+        });
+
+        var incorectAnswerDescriptionText = new Kinetic.Text({
+            x: 150,
+            y: 240,
+            text: incorrectAnswerDescription,
+            fontSize: 20,
+            fontFamily: 'Calibri',
+            width: 500,
+            align: 'center',
+            fill: '#1D7074',
+        });
+
         var nextQuestion = new Kinetic.Rect({
             x: 250,
             y: 500,
@@ -93,7 +170,8 @@
             nextQuestion.fill('red');
             layer.draw();
         })
-        layer.add(nextQuestion, nextQuestionText, nextQuestionInvisible);
+
+        layer.add(nextQuestion, nextQuestionText, nextQuestionInvisible, incorectedAnswerBox, incorectAnswerText, incorectAnswerDescriptionText);
         stage.add(layer);
     };
 
