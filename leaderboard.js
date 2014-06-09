7f63a3a46b1de31b120d5ae9765d0c236b0ba88c
Stefan Dimov <kratoz333@gmail.com>
2014-06-09 22:14:24 +0300

    Leaderboard commit (no styles)

--- leaderboard.js
+++ leaderboard.js
@@ -1,30 +1,69 @@
 (function() {
     var LEADERBOARD_KEY = 'key';
 
-    var leaderboard = {
-        0: {
-            name: 'unknown',
-            score: 0
-        },
-        1: {
-            name: 'unknown',
-            score: 0
-        },
-        2: {
-            name: 'unknown',
-            score: 0
-        },
-        3: {
-            name: 'unknown',
-            score: 0
-        },
-        4: {
-            name: 'unknown',
-            score: 0
+    if (localStorage.getItem(LEADERBOARD_KEY) == null) {
+        var leaderboard = {
+            0: {
+                name: 'unknown',
+                score: 0
+            },
+            1: {
+                name: 'unknown',
+                score: 0
+            },
+            2: {
+                name: 'unknown',
+                score: 0
+            },
+            3: {
+                name: 'unknown',
+                score: 0
+            },
+            4: {
+                name: 'unknown',
+                score: 0
+            }
         }
-    }
 
-    localStorage.setItem(LEADERBOARD_KEY, JSON.stringify(leaderboard));
+        localStorage.setItem(LEADERBOARD_KEY, JSON.stringify(leaderboard));
+    }
+    
+    setLeaderboard();
+
+    function setLeaderboard() {
+        var leaderboard = getLeaderboard();
+        
+        var playerArr = [];
+        
+        for (var player in leaderboard) {
+            playerArr.push(leaderboard[player]);
+        }
+        
+        playerArr.sort(function(a, b) {
+            return b.score - a.score;
+        });
+        
+             
+        var list = document.createElement('ul');
+        
+        var i;
+        var listItem;
+        var name;
+        var score;
+        var position;
+        for (i = 0; i < playerArr.length; i += 1) {
+            position = (i + 1).toString();
+            name = playerArr[i].name;
+            score = playerArr[i].score;
+            listItem = document.createElement('li');
+            
+            listItem.innerHTML = position + '. ' + name + ' ' + score;
+            list.appendChild(listItem);
+        }
+        
+        var targetDiv = document.getElementById('leaderboard-screen');
+        targetDiv.appendChild(list);
+    }
 
     function getLeaderboard() {
         var leaderboardString = localStorage.getItem(LEADERBOARD_KEY);
