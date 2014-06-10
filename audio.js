59a5e8d909b8aec7189c289a7ff0c7440c595c00
Stefan Dimov <kratoz333@gmail.com>
2014-06-10 13:04:55 +0300

    Mute/Unmute Button Styles Added

--- audio.js
+++ audio.js
@@ -1,6 +1,6 @@
 function muteAudio() {
     document.getElementById('audio').muted = !document.getElementById('audio').muted;
-    var btnAudio = document.getElementById('audioControls');
+    var btnAudio = document.getElementById('audioControl');
     if (btnAudio.innerHTML === 'Mute') {
         btnAudio.innerHTML = 'Unmute';
     } else {
