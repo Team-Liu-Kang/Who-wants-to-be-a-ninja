8141556a07d32588e7ca97201d8513fc12a78117
kris4o1993 <kris4o1993@gmail.com>
2014-06-10 12:53:35 +0300

    minor fix
    minor fix

---
+++ audio.js
@@ -0,0 +1,9 @@
+function muteAudio() {
+    document.getElementById('audio').muted = !document.getElementById('audio').muted;
+    var btnAudio = document.getElementById('audioControls');
+    if (btnAudio.innerHTML === 'Mute') {
+        btnAudio.innerHTML = 'Unmute';
+    } else {
+        btnAudio.innerHTML = 'Mute'
+    }
+}
