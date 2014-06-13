93ea9e7a75b01d2b23e572fb999a88ead7f69341
Simeon Tzvetkov <simeon_tzvetkov@mail.bg>
2014-06-13 21:58:59 +0300

    Field "name" added to the start screen.

--- Events.js
+++ Events.js
@@ -1,6 +1,8 @@
 ﻿$('#new-game-button').on('click', function () {
-    $('#wrapper').fadeOut(1000);
-    gameEngine.nextQuestion();
+    if (gameEngine.isValid($('#input-name'))) {
+        $('#wrapper').fadeOut(1000);
+        gameEngine.nextQuestion();
+    }   
 })
 $('#leaderboard-button').on('click', function () {
     $('#leaderboard-screen').fadeIn(1000);
